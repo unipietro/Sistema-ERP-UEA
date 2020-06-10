83ee928a19aca48d03660d8978662e882ca7469f
unipietro <64235362+unipietro@users.noreply.github.com>
2020-06-09 22:40:37 -0300

    alteração pra geral

--- Form1.cs
+++ Form1.cs
@@ -166,7 +166,7 @@ namespace WindowsFormsApp1
         {
             ActivateButton(sender, RGBColors.color4);
             OpenChildForm(new FormVendedores());
-            lblTitleChildForm.Text = "VENDEDORES";
+            lblTitleChildForm.Text = "VENDEDORES00001";
         }
 
         private void btnFinancas_Click(object sender, EventArgs e)
