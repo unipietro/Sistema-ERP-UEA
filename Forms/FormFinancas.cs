ad8ba81aff5239abb5880bd1af024f58da1d43b3
unipietro <64235362+unipietro@users.noreply.github.com>
2020-06-11 21:18:56 -0300

    Atualização dashboard

--- Forms/FormFinancas.cs
+++ Forms/FormFinancas.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1.Forms
         {
             InitializeComponent();
         }
+
+        private void FormFinancas_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
